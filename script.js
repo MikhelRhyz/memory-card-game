1d68a172ebd530f79bb4babf92b1e8b26eb6c789
Rhyz <mikhelrhyz@gmail.com>
2025-10-15 16:27:05 +0800

    change selected option

--- script.js
+++ script.js
@@ -6,37 +6,30 @@ const matches = document.querySelector("#matches");
 let matchCount = 0;
 const difficulty = document.querySelector("#difficulty");
 const board = document.querySelector("#board");
-
 cardInners.forEach((item) =>
   item.addEventListener("click", () => {
     item.closest(".card").classList.toggle("is-flipped");
     countMove(item);
   })
 );
-
 function countMove(item) {
   moveCounter++;
   moves.textContent = moveCounter;
   picks.push(item.querySelector("img").className);
-
   if (moveCounter % 2 === 0) {
     setTimeout(() => {
       cardInners.forEach((card) =>
         card.closest(".card").classList.remove("is-flipped")
       );
     }, 800);
-
     if (picks[0] === picks[1]) {
       matchCount++;
       matches.textContent = matchCount;
-
       picks = [];
     }
   }
-
-  console.log(moveCounter)
+  console.log(moveCounter);
 }
-
 difficulty.addEventListener("change", () => {
   if (difficulty.value === "easy") {
     for (let i = 10; i > 0; i--) {
@@ -45,41 +38,31 @@ difficulty.addEventListener("change", () => {
       divCard.role = "button";
       divCard.tabIndex = "0";
       divCard.ariaPressed = "false";
-
       const divCardInner = document.createElement("div");
       divCardInner.className = "card__inner";
-
       const divCardFront = document.createElement("div");
       divCardFront.className = "card__front";
       divCardFront.ariaHidden = "true";
-
       const img = document.createElement("img");
       img.className = "icon-2";
       img.alt = "icon-2";
-
       const divCardBack = document.createElement("div");
       divCardBack.className = "card__back";
-
       const logo = document.createElement("span");
       logo.className = "logo";
       logo.textContent = "?";
-
       divCardFront.appendChild(img);
       divCardBack.appendChild(logo);
       divCardInner.appendChild(divCardFront);
       divCardInner.appendChild(divCardBack);
       divCard.appendChild(divCardInner);
       board.appendChild(divCard);
-
       divCard.addEventListener("click", () => {
         divCard.classList.toggle("is-flipped");
-
-        countMove(divCardInner); 
+        countMove(divCardInner);
       });
     }
-
     cardInners = document.querySelectorAll(".card__inner");
   }
-
   console.log(difficulty.value);
 });
